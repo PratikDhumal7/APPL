7ab60aad1a3297220459db81b506345b17efaa90
Leo <leorjjj1@gmail.com>
2022-01-27 11:02:46 +0530

    Add header files

---
+++ stuff/lib_csv.h
@@ -0,0 +1,6 @@
+#ifndef lib_csv
+#define lib_csv
+#endif
+#include "lib_matrix.h"
+
+struct Matrix* read_csv(char*);
---
+++ stuff/lib_matrix.h
@@ -0,0 +1,31 @@
+#ifndef lib_matrix
+#define lib_matrix
+#endif
+
+struct Matrix{
+    int r_size;
+    int c_size;
+    double* data;
+};
+
+struct Matrix* mat_init(int, int, int, double *);
+
+struct Matrix* mat_initEmpty(int, int);
+
+struct Matrix* mat_initRandom(int, int, int);
+
+void mat_del(struct Matrix*);
+
+int mat_getSize(struct Matrix*);
+
+int mat_add(struct Matrix*, struct Matrix*, struct Matrix*);
+
+void mat_addScalar(struct Matrix*, double);
+
+int mat_dot(struct Matrix*, struct Matrix*, struct Matrix*);
+
+void mat_append(struct Matrix*, int, double*);
+
+void mat_reshape(struct Matrix*, int, int);
+
+void mat_display(struct Matrix*);
